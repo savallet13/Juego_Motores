b50e74582b6a2258217d6b762ee875a23c36a65c
emiliobort <cualesmiemail@gmail.com>
2017-04-04 13:36:26 +0200

    Enemy Movement
    Movement Enemy

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Animator anim;
+    Transform player;               // Reference to the player's position.
+    //PlayerHealth playerHealth;      // Reference to the player's health.
+    //EnemyHealth enemyHealth;        // Reference to this enemy's health.
+    NavMeshAgent nav;               // Reference to the nav mesh agent.
+
+
+    void Awake()
+    {
+        // Set up the references.
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //playerHealth = player.GetComponent<PlayerHealth>();
+        //enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Correr()
+    {
+        anim.SetBool("Visto", true);
+    }
+
+
+    void Update()
+    {
+        
+        nav.SetDestination(player.position);
+
+        Correr();
+        /*
+        // If the enemy and the player have health left...
+        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+            // ... set the destination of the nav mesh agent to the player.
+            
+        }
+        // Otherwise...
+        else
+        {
+            // ... disable the nav mesh agent.
+            nav.enabled = false;
+        }*/
+    }
+}
