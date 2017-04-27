07213f7da7ace8d2c54b6d76bd9c6fe4b8ac8390
savallet13 <savallet13@hotmail.com>
2017-04-27 20:29:56 +0200

    V3_HUD
    First version Scritp HUD more updates details HUD

---
+++ Assets/Scripts/Hud/Hud.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hud : MonoBehaviour {
+
+    //Publics
+    public Slider slider_life;
+    public Slider slider_food;
+    public Text count_pieces;
+    //Private
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        setFood();
+        setLife();
+        
+    }
+    void setLife()
+    {
+        if(slider_life.value <= 0)
+        {
+            //You has die!!
+
+        }
+        else
+        {
+            slider_life.value -= 0.01f;
+        }
+
+        
+    }
+    void setFood()
+    {
+        if (slider_food.value  <= 0)
+        {
+            //You has angry, need eat!!
+            slider_food.value = 0;
+            slider_life.value -= 0.05f;
+
+        }
+        else
+        {
+            slider_food.value -= 0.25f;
+        }
+        
+    }
+}
