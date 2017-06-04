7f60606e67606d82f1a484e049ff5c84d5535dcd
emiliobort <cualesmiemail@gmail.com>
2017-06-04 17:06:05 +0400

    Menu Options and End
    Back end buttons options

---
+++ Assets/UI/Fin.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Fin : MonoBehaviour
+{
+
+    public Texture2D Background;
+
+    
+
+
+
+    void Update()
+    {
+    }   
+
+    void OnGUI()
+    {
+        SGUI.DrawTexture(0, 0, 100, 100, Background);
+        
+        
+        
+    }
+}
---
+++ Assets/UI/Options.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Options : MonoBehaviour
+{
+
+    public Texture2D Background;
+
+    public Texture2D OnT;
+
+    public Texture2D OffT;
+
+    public Texture2D BackT;
+
+
+    SGUI.SelfButton On;
+    SGUI.SelfButton Off;
+    SGUI.SelfButton Back;
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (SGUI.PixelInPercentages(Input.mousePosition, On.Percentages))
+            {
+                //Encender Musica y sonido
+
+
+            }
+
+            if (SGUI.PixelInPercentages(Input.mousePosition, Off.Percentages))
+            {
+                //Apagar Musica y sonido
+
+            }
+
+            if (SGUI.PixelInPercentages(Input.mousePosition, Back.Percentages))
+            {
+                SceneManager.LoadScene("Menu");
+
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        SGUI.DrawTexture(0, 0, 100, 100, Background);
+        
+        float size = 15;
+        float sizeb = 13;
+        float sizec = 7;
+        On = new SGUI.SelfButton(42, 50, size, size, OnT, 1.1f, 1.1f);
+        Off = new SGUI.SelfButton(42, 65, size, sizeb, OffT, 1.1f, 1.1f);
+        Back = new SGUI.SelfButton(90, 80, sizec, sizec, BackT, 1.1f, 1.1f);
+        
+
+
+        On.Draw();
+        Off.Draw();
+        Back.Draw();
+    }
+}
