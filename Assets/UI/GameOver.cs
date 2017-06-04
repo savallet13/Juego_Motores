cebc1fc5c98805bfdcc4c8b1df1c4487c27f9b36
emiliobort <cualesmiemail@gmail.com>
2017-06-04 18:03:13 +0400

    GameOver Screen
    Screen gameover with button back

---
+++ Assets/UI/GameOver.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+    public Texture2D Background;
+
+    public Texture2D BackT;
+
+
+    SGUI.SelfButton Back;
+    
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (SGUI.PixelInPercentages(Input.mousePosition, Back.Percentages))
+            {
+                SceneManager.LoadScene("Menu");
+
+
+            }
+            
+        }
+    }
+
+    void OnGUI()
+    {
+        SGUI.DrawTexture(0, 0, 100, 100, Background);
+        
+        float size = 7;
+        Back = new SGUI.SelfButton(50, 70, size, size, BackT, 1.0f, 1.1f);
+        
+        
+
+
+        Back.Draw();
+        
+    }
+}
